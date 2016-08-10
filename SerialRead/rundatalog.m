845a6265fe542da15c41034b2b73a4fcb2382579
evancs <evancs@gmail.com>
2016-08-09 21:11:13 -0700

    fixed rundatalog to stream
    added a pause and correctly used BytesAvailable

--- SerialRead/rundatalog.m
+++ SerialRead/rundatalog.m
@@ -6,47 +6,42 @@ function [rawdata_exp] = rundatalog(fname)
 % utilizes bin2Q, much faster 2's complement binary to dec converter
 % for serial in general
 
-com = 'COM21'; % specify com channel
-
-% MATCH BAUDRATE WITH XBEE!!!
-bdr = 38400;
-bfs_exp = 50;
-
 % create serial object
-s_exp = serial(com,'BaudRate',bdr,'DataBits',8,'StopBits',1,'InputBufferSize',bfs_exp, 'Parity', 'none')
+com = 'COM21'; % specify com channel
+bdr = 38400; % MATCH BAUDRATE WITH XBEE!!!
+bfs_exp = 44; % =datapacketsize * number of nodes, i.e. 22 x 2 = 44
+s_exp = serial(com,'BaudRate',bdr,'DataBits',8,'StopBits',1,'InputBufferSize',bfs_exp, 'Parity', 'none');
 
 % open serial channel
-fopen(s_exp)
+fopen(s_exp);
 
 %initialize
-rawdata_exp = zeros(300,1);
+rawdata_exp = zeros(bfs_exp,10);
 
 tic
-
 % Read data streaming from XBEE
-for i = 1:300
+for i = 1:10
     try
-        rawdata_exp(i) = fread(s_exp,1,'uchar'); % extract 1 byte
+        if s_exp.BytesAvailable>0
+            datasize = s_exp.BytesAvailable;
+            rawdata_exp(1:datasize,i)= fread(s_exp,datasize,'uchar');           
+        end
+        pause(.5); %pause time set to the same period as Arduino sending rate
     catch ME
         fclose(s_exp)
         throw(ME)
-    end
-    
+    end    
 end
-
 toc
 
-tic
 % Close serial object
 fclose(s_exp)
 delete(s_exp)
 clear s_exp
-toc
 
-tic
 % Convert serial data:
-data_exp = serial2datan(rawdata_exp)
-toc
+%data_exp = serial2datan(rawdata_exp)
+
 % Save data:
 % data_exp(:,5)=data_exp(:,5)/100; %Accmag*100
 % data_exp(:,6)=data_exp(:,6)/100; %gz*100
